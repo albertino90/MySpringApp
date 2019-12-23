8555eeb6d947c6481fb1df9ca2aba5e9e45686d9
Albert N <unilon90@mail.ru>
2019-12-23 18:23:59 +0500

    entity to dto  - builder changed to mapstruct  - fix lombok and mapstruct conflict in annotationProcessorPaths

--- src/main/java/com/springtest/controllers/UserController.java
+++ src/main/java/com/springtest/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.springtest.controllers;
 
 import com.springtest.dto.RoleDTO;
 import com.springtest.dto.UserDTO;
+import com.springtest.mappers.UserMapper;
 import com.springtest.model.Role;
 import com.springtest.model.User;
 import com.springtest.services.UserService;
@@ -26,29 +27,12 @@ public class UserController {
 //    @PathVariable - Аннотация, которая показывает, что параметр метода должен быть связан с переменной из урл-адреса.
     @GetMapping("/users/{id}")
     public UserDTO sayHello(@PathVariable Long id){
-//        return userService.getUser(id);
         User user = userService.getUser(id);
-        return toDTO(user);
+//        return toDTO(user);
+        return UserMapper.INSTANCE.toDTO(user);
     }
 
-    private UserDTO toDTO(User user){
-        Set<RoleDTO> roleDTOS = toDTOs(user.getRoles());
-        return UserDTO.builder()
-                .id(user.getId())
-                .name(user.getName())
-                .email(user.getEmail())
-                .roles(roleDTOS)
-                .build();
-    }
-    private Set<RoleDTO> toDTOs(Set<Role> role){
-        return role.stream().map(role1 -> toDTO(role1)).collect(Collectors.toSet());
-    }
-    private RoleDTO toDTO(Role role){
-        return RoleDTO.builder()
-                .id(role.getId())
-                .name(role.getName())
-                .build();
-    }
+
 }
 
 
--- src/main/java/com/springtest/mappers/UserMapper.java
+++ src/main/java/com/springtest/mappers/UserMapper.java
@@ -1,14 +1,14 @@
-//package com.springtest.mappers;
-//
-//import com.springtest.dto.UserDTO;
-//import com.springtest.model.User;
-//import org.mapstruct.Mapper;
-//import org.mapstruct.factory.Mappers;
-//
-//@Mapper
-//public interface UserMapper {
-//
-//    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
-//
-//    UserDTO toDTO(User user);
-//}
+package com.springtest.mappers;
+
+import com.springtest.dto.UserDTO;
+import com.springtest.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserDTO toDTO(User user);
+}
